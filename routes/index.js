3b349a5f08ff4dcf57e3b3b4e664a6d30c23e7e6
Pawel <pawel.siwek@filament.uk.com>
2021-09-30 16:08:28 +0100

    added addEmployee to routes in correct way

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
-
+const addEmployee = require('../controller/addEmployee')
 const Router = express.Router()
+Router.post('/Employee', addEmployee.newEmployee)
 
 module.exports = Router
