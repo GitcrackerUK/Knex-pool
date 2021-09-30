61b53a076bd75eebf4458abd9cd643cde8996157
Pawel <pawel.siwek@filament.uk.com>
2021-09-30 16:28:51 +0100

    added timeout

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
-const express = require('express')
 const addEmployee = require('../controller/addEmployee')
+const timeout = require('express-timeout-handler');
+const express = require('express')
+
 const Router = express.Router()
-Router.post('/Employee', addEmployee.newEmployee)
+Router.post('/Employee', timeout.set(4000), addEmployee.newEmployee)
 
 module.exports = Router
