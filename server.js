b63ad1ee40afa9ccfaae9d165dc783ed9cd91cce
Pawel <pawel.siwek@filament.uk.com>
2021-09-30 12:09:32 +0100

    added router dir with index + app.use(router)

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-const express = require('express')
+const express = require('express');
+const router = require('./routes')
 const app = express()
 const port = 3000
 
 
-app.use()
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(router)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
