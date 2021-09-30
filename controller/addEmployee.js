89f3e60468ccc0a2e150413efe73cf7cc3173ab8
Pawel <pawel.siwek@filament.uk.com>
2021-09-30 17:43:10 +0100

    changged import of service methods

--- controller/addEmployee.js
+++ controller/addEmployee.js
@@ -1,11 +1,12 @@
-const EmployeeService = require('../services')
+const services = require('../services')
+const EmployeeService = new services.EmployeeService
 const createEmployee = require('./createEmployee')
 
 class employeeController{
   async  newEmployee(req,res){
      try{
-        const id = await EmployeeService.newEmployee(createEmployee())
-        res.status(201).json({message: "user added",id})
+        const uid = await EmployeeService.newEmployee(createEmployee())
+        res.status(201).json({message: "user added", uid})
     }catch(err){
          console.error(err)
      }
