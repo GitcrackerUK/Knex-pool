942d46ac27c0821cf5c3f58a03d4fec9263b7ee9
Pawel <pawel.siwek@filament.uk.com>
2021-09-30 22:49:18 +0100

    renamed to Post related

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const addEmployee = require('../controller/addEmployee')
-const changeTable = require('../controller/changeTable')
+const addPost = require('../controller/addPost')
 const timeout = require('express-timeout-handler');
 const express = require('express')
 
 const Router = express.Router()
 Router.post('/Employee', timeout.set(4000), addEmployee.newEmployee)
-Router.post('/AddRow', timeout.set(4000), changeTable.addRow)
+Router.post('/AddPost', timeout.set(4000), addPost.addPostToTable)
 
 module.exports = Router
