b4ef8d999d270c12bc5dea917bc687393f196d84
Pawel <pawel.siwek@filament.uk.com>
2021-09-30 16:29:47 +0100

    added timeout to server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
+const timeout = require('express-timeout-handler');
 const express = require('express');
 const router = require('./routes')
 const app = express()
 const port = 3000
 
 
-app.use(router)
 
+app.use(router)
+app.use(timeout.handler());
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
