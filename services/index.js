3b98cbc0b22a67b2900a8829080c64ac8ba16ec6
Pawel <pawel.siwek@filament.uk.com>
2021-09-30 17:41:01 +0100

    added TableService

--- services/index.js
+++ services/index.js
@@ -1,17 +1,28 @@
-const db = require('../db')
+const db = require('../db');
 
-class EmployeeService{
-   async newEmployee({uid, firstName, lastName, email, sse, gender}){
-       await  db('Employees').insert({
+class EmployeeService {
+    async newEmployee({ uid, firstName, lastName, email, sse, gender }) {
+        await db('Employees').insert({
             uid: uid,
             first_name: firstName,
             surname_name: lastName,
             email: email,
             sse: sse,
-            gender: gender
-        })
-        return uid
+            // gender: gender,
+        });
+        return uid;
     }
 }
 
-module.exports = new EmployeeService()
+class TableService {
+    async addNewRowToTable({ rowName }) {
+        db.table('Employees', function (table) {
+            table.string(rowName);
+        });
+    }
+}
+
+module.exports = { 
+    EmployeeService: EmployeeService,
+     TableService: TableService 
+    };
